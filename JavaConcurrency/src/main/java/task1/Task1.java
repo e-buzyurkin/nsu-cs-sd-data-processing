58afee5a4f0050790421c56b573809ee52737a16
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-09-11 20:19:40 +0700

    initial commit

---
+++ JavaConcurrency/src/main/java/task1/Task1.java
@@ -0,0 +1,20 @@
+package task1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("new thread says " + i);
+            }
+        };
+
+        Thread thread = new Thread(task);
+
+        thread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("old thread says " + i);
+        }
+    }
+}
