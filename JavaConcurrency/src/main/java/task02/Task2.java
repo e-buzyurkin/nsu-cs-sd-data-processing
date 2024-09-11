b004096fb106c25fe5a4615f65445068f95db04a
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-09-11 20:32:04 +0700

    task 2

---
+++ JavaConcurrency/src/main/java/task02/Task2.java
@@ -0,0 +1,27 @@
+package task02;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("new thread says " + i);
+            }
+        };
+
+        Thread thread = new Thread(task);
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("new thread finished");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("old thread says " + i);
+        }
+    }
+}
