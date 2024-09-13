ad52ed07a8e95d3f852e1683aded1e116acfc72b
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-09-13 14:08:08 +0700

    not sure

--- JavaConcurrency/src/main/java/task03/Task3.java
+++ JavaConcurrency/src/main/java/task03/Task3.java
@@ -3,32 +3,43 @@ package task03;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Task3 {
     static Random rand = new Random();
     
     public static void main(String[] args) {
-        List<Runnable> tasks = new ArrayList<>();
+        List<String> strings = new CopyOnWriteArrayList<>();
+        strings.add("3");
+        Thread thread1 = new Thread(new Printer("alpha", strings));
+        thread1.start();
 
-        for (int i = 0; i < 4; i++) {
-            tasks.add(new Printer(List.of(i + "-bobik", i + "-murzik", i + "-sharik")));
-        }
+        strings.add("5");
+        Thread thread2 = new Thread(new Printer("beta", strings));
+        thread2.start();
 
-        for (var task : tasks) {
-            Thread thread = new Thread(task);
-            thread.start();
-        }
+        strings.add("7");
+        Thread thread3 = new Thread(new Printer("gamma", strings));
+        thread3.start();
+
+        strings.add("11");
+        Thread thread4 = new Thread(new Printer("delta", strings));
+        thread4.start();
     }
 
     private static class Printer implements Runnable {
+        private String name;
         private List<String> list;
 
-        public Printer(List<String> list) {
-            this.list = list;
+        public Printer(String name, List<String> list) {
+            this.name = name;
+            this.list = new ArrayList<>(list);
         }
 
         public void run() {
-            list.forEach(System.out::println);
+            for (var elem : list) {
+                System.out.println(name + "-" + elem);
+            }
         }
     }
 }
