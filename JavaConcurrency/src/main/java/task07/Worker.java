5118e5b21992d4b59b4076dcaa1519f36c1ed0c2
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-09-25 23:54:07 +0700

    task7

---
+++ JavaConcurrency/src/main/java/task07/Worker.java
@@ -0,0 +1,25 @@
+package task07;
+
+public class Worker extends Thread {
+    int from;
+    int to;
+    double result = 0.0;
+
+    public Worker(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    @Override
+    public void run() {
+        for (int i = from; i < to; i++) {
+            result += Math.pow(-1, i) / (2 * i + 1);
+        }
+
+        result *= 4.0;
+    }
+}
