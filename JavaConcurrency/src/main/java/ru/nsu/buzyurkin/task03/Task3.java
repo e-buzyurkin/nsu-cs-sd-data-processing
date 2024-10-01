e25862cc76015f991c1279e59d32ac5348d1ef35
Evgeniy Buzyurkin <115026888+e-buzyurkin@users.noreply.github.com>
2024-10-01 09:11:12 +0700

    task 03 (#3)
    * task3
    
    * not sure
    
    * package fix

---
+++ JavaConcurrency/src/main/java/ru/nsu/buzyurkin/task03/Task3.java
@@ -0,0 +1,45 @@
+package ru.nsu.buzyurkin.task03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Task3 {
+    static Random rand = new Random();
+    
+    public static void main(String[] args) {
+        List<String> strings = new CopyOnWriteArrayList<>();
+        strings.add("3");
+        Thread thread1 = new Thread(new Printer("alpha", strings));
+        thread1.start();
+
+        strings.add("5");
+        Thread thread2 = new Thread(new Printer("beta", strings));
+        thread2.start();
+
+        strings.add("7");
+        Thread thread3 = new Thread(new Printer("gamma", strings));
+        thread3.start();
+
+        strings.add("11");
+        Thread thread4 = new Thread(new Printer("delta", strings));
+        thread4.start();
+    }
+
+    private static class Printer implements Runnable {
+        private String name;
+        private List<String> list;
+
+        public Printer(String name, List<String> list) {
+            this.name = name;
+            this.list = new ArrayList<>(list);
+        }
+
+        public void run() {
+            for (var elem : list) {
+                System.out.println(name + "-" + elem);
+            }
+        }
+    }
+}
