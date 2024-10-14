3fc6b74c5dfe379d4f494e5293752e73b606c862
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-10-14 21:59:19 +0700

    strings now split into 50char chunks before adding to list

--- JavaConcurrency/src/main/java/ru/nsu/buzyurkin/task12/Main.java
+++ JavaConcurrency/src/main/java/ru/nsu/buzyurkin/task12/Main.java
@@ -1,9 +1,11 @@
 package ru.nsu.buzyurkin.task12;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static final int PART_LENGTH = 50;
     private static final LinkedList<String> list = new LinkedList<>();
 
     public static void main(String[] args) {
@@ -18,7 +20,11 @@ public class Main {
                 if (input.isEmpty()) {
                     list.forEach(System.out::println);
                 } else {
-                    list.addFirst(input);
+                    for (int i = 0; i < input.length(); i += PART_LENGTH) {
+                        String sub = input.substring(i, Math.min(i + PART_LENGTH, input.length()));
+
+                        list.addFirst(sub);
+                    }
                 }
             }
         }
