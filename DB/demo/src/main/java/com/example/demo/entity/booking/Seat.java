38706d3ecfbad41172f484668e8376459f757346
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2025-06-11 15:10:41 +0700

    Revert "dddd"
    This reverts commit 511eb41ac6b5aa6b65c6c49285f09f852ee741fd.

--- DB/demo/src/main/java/com/example/demo/entity/booking/Seat.java
+++ DB/demo/src/main/java/com/example/demo/entity/booking/Seat.java
@@ -1,12 +1,14 @@
 package com.example.demo.entity.booking;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import lombok.Data;
 
 @Entity
 @Data
 @IdClass(SeatId.class)
-@Table(name = "seats")
 public class Seat {
 	@Id
 	@Column(name = "aircraft_code")
